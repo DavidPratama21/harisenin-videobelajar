b2cceaaa347ccf58f2efb3532c263d02464258f8
Daun2121 <rayruby210203@gmail.com>
2025-08-03 16:05:07 +0700

    jwt done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 import {
     pool,
     getProducts,
@@ -13,6 +14,8 @@ import {
     findUserById,
     updateUser,
 } from "./src/configs/database.js";
+
+const JWT_SECRET = "rahasia";
 const app = express();
 app.use(
     cors({
@@ -21,9 +24,129 @@ app.use(
 );
 app.use(express.json());
 
+// ===== USERS =====
+
+app.post("/register", async (req, res) => {
+    const { name, email, phone, gender, password, avatar } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).send({ message: "Data tidak lengkap!" });
+    }
+
+    const [check] = await pool.query(`SELECT * FROM users WHERE email = ?`, [
+        email,
+    ]);
+    if (check.length > 0) {
+        return res.status(409).send("Email sudah terdaftar");
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+    const userId = await createUser({
+        name,
+        email,
+        phone,
+        gender,
+        password: hash,
+        avatar,
+    });
+
+    res.status(201).send({
+        message: "Registrasi berhasil",
+        user_id: userId,
+    });
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).send({ message: "Isi Email & Password nya!" });
+    }
+
+    const user = await findUserByEmail(email);
+    if (!user) {
+        return res.status(404).send("Email ga ketemu");
+    }
+
+    console.log(user);
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        return res.status(401).json({ message: "Invalid Password" });
+    }
+
+    const token = jwt.sign({ id: user.user_id }, JWT_SECRET, {
+        expiresIn: "1h",
+    });
+    res.send({
+        message: "Login Success",
+        token,
+        user: {
+            id: user.user_id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+        },
+    });
+});
+
+const autheticateToken = (req, res, next) => {
+    const token = req.headers["authorization"]?.split("")[1];
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+app.get("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await findUserById(id);
+        if (!user) {
+            return res.status(404).send("User tidak ditemukan");
+        }
+        res.send({
+            success: true,
+            data: user,
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: "Gagal mengambil data user",
+            error: e.message,
+        });
+    }
+});
+
+app.put("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const { name, email, phone, avatar, password } = req.body;
+    try {
+        const [emailTerdaftar] = await pool.query(
+            `SELECT user_id FROM users WHERE email = ? AND user_id !=?`,
+            [email, id]
+        );
+        if (emailTerdaftar.length > 0) {
+            return res.status(400).send({ message: "Email sudah terdaftar" });
+        }
+
+        let hash = null;
+        if (password) {
+            hash = await bcrypt.hash(password, 10);
+        }
+        await updateUser({ name, email, phone, avatar, password: hash }, id);
+        res.send({ success: true, message: "User berhasil diupdate" });
+    } catch (e) {
+        res.status(500).send({
+            message: "Gagal mengupdate data user",
+            error: e.message,
+        });
+    }
+});
+
 // ===== PRODUCTS =====
 
-app.get("/products", async (req, res) => {
+app.get("/products", autheticateToken, async (req, res) => {
     const products = await getProducts();
     res.send(products);
 });
@@ -90,111 +213,4 @@ app.use((err, req, res, next) => {
 
 app.listen(8080, () => {
     console.log("Lagi di port 8080");
-});
-
-// ===== USERS =====
-
-app.post("/register", async (req, res) => {
-    const { name, email, phone, gender, password, avatar } = req.body;
-    if (!name || !email || !password) {
-        return res.status(400).send({ message: "Data tidak lengkap!" });
-    }
-
-    const [check] = await pool.query(`SELECT * FROM users WHERE email = ?`, [
-        email,
-    ]);
-    if (check.length > 0) {
-        return res.status(409).send("Email sudah terdaftar");
-    }
-
-    const hash = await bcrypt.hash(password, 10);
-    const userId = await createUser({
-        name,
-        email,
-        phone,
-        gender,
-        password: hash,
-        avatar,
-    });
-
-    res.status(201).send({
-        message: "Registrasi berhasil",
-        user_id: userId,
-    });
-});
-
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).send({ message: "Isi Email & Password nya!" });
-    }
-
-    const user = await findUserByEmail(email);
-    if (!user) {
-        return res.status(404).send("Email ga ketemu");
-    }
-    
-    console.log(user)
-
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-        return res.status(401).json({ message: "Invalid Password" });
-    }
-    res.send({
-        message: "Login Success",
-        user: {
-            id: user.user_id,
-            name: user.name,
-            email: user.email,
-            avatar: user.avatar,
-        },
-    });
-});
-
-app.get("/users/:id", async (req, res) => {
-    const id = req.params.id;
-    try {
-        const user = await findUserById(id);
-        if (!user) {
-            return res.status(404).send("User tidak ditemukan");
-        }
-        res.send({
-            success: true,
-            data: user,
-        });
-    } catch (e) {
-        res.status(500).send({
-            message: "Gagal mengambil data user",
-            error: e.message,
-        });
-    }
-});
-
-app.put("/users/:id", async (req, res) => {
-    const id = req.params.id;
-    const { name, email, phone, avatar, password } = req.body;
-    try {
-        const [emailTerdaftar] = await pool.query(
-            `SELECT user_id FROM users WHERE email = ? AND user_id !=?`,
-            [email, id]
-        );
-        if (emailTerdaftar.length > 0) {
-            return res.status(400).send({ message: "Email sudah terdaftar" });
-        }
-
-        let hash = null;
-        if (password) {
-            hash = await bcrypt.hash(password, 10);
-        }
-        await updateUser(
-            { name, email, phone, avatar, password: hash },
-            id
-        );
-        res.send({ success: true, message: "User berhasil diupdate" });
-    } catch (e) {
-        res.status(500).send({
-            message: "Gagal mengupdate data user",
-            error: e.message,
-        });
-    }
 });
